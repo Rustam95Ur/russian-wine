37120a8ee5c13878f6d4d84fa69f05944c709996
Kurbanov Rustam <kurbanov.rustam@applecity.kz>
2020-07-31 13:14:30 +0600

    Updated table name

--- database/migrations/2020_07_29_190227_create_favorites_table.php
+++ database/migrations/2020_07_29_190227_create_favorites_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFavoriteTable extends Migration
+class CreateFavoritesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateFavoriteTable extends Migration
      */
     public function up()
     {
-        Schema::create('favorite', function (Blueprint $table) {
+        Schema::create('favorites', function (Blueprint $table) {
             $table->id();
             $table->integer('client_id');
             $table->integer('wine_id');
@@ -28,6 +28,6 @@ class CreateFavoriteTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('favorite');
+        Schema::dropIfExists('favorites');
     }
 }
