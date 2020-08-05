4f064c8c27abef6c710b4b635f7ecbe041f75df0
Пеливан Эдуард Владимирович <eduard.pelivan@resmi.kz>
2020-08-06 02:41:13 +0600

    profile modified, added left-side-menu layout

--- app/Http/Controllers/Page/FavoriteController.php
+++ app/Http/Controllers/Page/FavoriteController.php
@@ -12,14 +12,13 @@ use Illuminate\Support\Facades\Auth;
  */
 class FavoriteController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth:client');
-    }
-
     // Только залогиненый пользователь можешь добавлять в избанные
     public function addToFavorite(Request $request)
     {
+        if (Auth::user() === null) {
+            return redirect()->route('home')->with('error', 'Только авторизованные пользователи могут добавлять в избранные');
+        }
+
         DB::table('client_wine')->insert(
             ['client_id' => Auth::user()->id, 'wine_id' => $request->wine_id]
         );
--- resources/views/page/profile/favorite.blade.php
+++ resources/views/page/profile/favorite.blade.php
@@ -49,49 +49,7 @@
     <div id="franchise">
         <div id="content">
             <div class="row">
-                <div class="col-md-4" style="background-color: rgb(44, 32, 48)">
-                    <div class="userData" style="margin: 15%;">
-                        <h3 style="color: white">{{Auth::user()->first_name}}</h3>
-
-                        <h4 class="heading">Скидка</h4>
-                        <h1 class="discount">5%</h1>
-
-                        <div class="row">
-                            <div class="col-md-8 col-sm-8">
-                                <ul>
-                                    <li class="rowlist"><a href="{{route('favorite')}}">Избранное</a></li>
-                                    <li class="rowlist"><a href="{{route('sub-wines')}}">Подписки</a></li>
-                                    <li class="rowlist"><a href="{{route('my-sets')}}">Сеты</a></li>
-                                    <li class="rowlist"><a href="{{route('my-orders')}}">Мои заказы</a></li>
-                                </ul>
-
-                                <ul class="additional">
-                                    <li class="rowlist"><a href="">Спецпредложения</a></li>
-                                    <li class="rowlist"><a href="{{route('profile')}}">Персональные данные</a></li>
-                                </ul>
-
-                                <div class="logout">
-                                    <a href="{{ route('logout') }}"
-                                       onclick="event.preventDefault(); document.getElementById('frm-logout').submit();">
-                                        Выйти
-                                    </a>
-                                    <form id="frm-logout" action="{{ route('logout') }}" method="POST"
-                                          style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </div>
-                            </div>
-                            <div class="col-md-4 col-sm-4">
-                                <ul class="right">
-                                    <li class="rowlist">0</li>
-                                    <li class="rowlist">2</li>
-                                    <li class="rowlist">5</li>
-                                    <li class="rowlist">15</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @include('page.profile.layouts.left-side-menu')
                 <div class="col-md-8">
                     @if (isset($favorites[0]))
                         <div style="margin: 30px;">
