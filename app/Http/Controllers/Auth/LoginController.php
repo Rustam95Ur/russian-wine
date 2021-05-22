c96208c12790d87c3fcc66025c4720f905252565
Пеливан Эдуард Владимирович <eduard.pelivan@resmi.kz>
2021-05-23 01:31:41 +0500

    redirect to prifile edited

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = "/profile";
+    protected $redirectTo = "/profile/favorite";
 
     /**
      * Create a new controller instance.
